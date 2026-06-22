455537134c45d4bff95906dfc5e295f0fe1f3274
Fp22z <fayrus.pena@unmsm.edu.pe>
2026-06-21 20:54:41 -0500

    feat: tercera prueba de implementacion

--- web-gelco/frontend/src/environments/environment.js
+++ web-gelco/frontend/src/environments/environment.js
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
-  url: 'http://localhost:8080/api/v1'
+  production: import.meta.env.PROD,
+  url: import.meta.env.VITE_API_URL || 'http://localhost:8080/api/v1'
 };
